635cbbfe1ed310cd65a29c9d8aced5e7c131b9fd
TiredClone <neolife145@gmail.com>
2023-11-24 01:59:02 +0300

    registration and auth

--- app/src/main/java/com/neolife/androidmessage/LoginActivity.java
+++ app/src/main/java/com/neolife/androidmessage/LoginActivity.java
@@ -2,13 +2,53 @@ package com.neolife.androidmessage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.annotations.NotNull;
+import com.google.firebase.ktx.Firebase;
+import com.neolife.androidmessage.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(binding.emailEdittext.getText().toString().isEmpty() || binding.passwordEdittext.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(binding.emailEdittext.getText().toString(), binding.passwordEdittext.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NotNull Task<AuthResult> task){
+                                    if (task.isSuccessful()){
+                                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                    }
+                                }
+                            })
+                ;}
+            }
+        });
+
+        binding.gtRegisterActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/neolife/androidmessage/RegisterActivity.java
+++ app/src/main/java/com/neolife/androidmessage/RegisterActivity.java
@@ -1,14 +1,56 @@
 package com.neolife.androidmessage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.neolife.androidmessage.databinding.ActivityRegisterBinding;
+
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.emailEdittext.getText().toString().isEmpty() || binding.passwordEdittext.getText().toString().isEmpty()
+                        || binding.usernameEdittext.toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.emailEdittext.getText().toString(), binding.passwordEdittext.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        HashMap<String,String> userInfo = new HashMap<>();
+                                        userInfo.put("email", binding.emailEdittext.getText().toString());
+                                        userInfo.put("username", binding.usernameEdittext.getText().toString());
+                                        FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(userInfo);
+                                        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
 }
